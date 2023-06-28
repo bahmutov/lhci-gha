5eced1502caa47a6b9d57b668e462208810f7863
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-06-28 13:34:43 -0400

    fix: round the performance number

--- src/read-report.js
+++ src/read-report.js
@@ -1,9 +1,15 @@
 const fs = require('fs')
 
+/**
+ * Reads the overall performance score and returns it from 1 to 100,
+ * rounded to the nearest whole number.
+ * @param {string} filename Lighthouse JSON report filename
+ * @returns
+ */
 function getPerformance(filename) {
   const results = JSON.parse(fs.readFileSync(filename))
   const performanceAudit = results.categories.performance
-  const performance = performanceAudit.score * 100
+  const performance = Math.round(performanceAudit.score * 100)
   return performance
 }
 
